5c9a3dcd67862c15c730fc829015fd3195ab2745
Qinx <me@qinxuzhou.com>
2023-09-06 21:09:12 -0600

    update batch size and epoch

--- finetune.py
+++ finetune.py
@@ -31,7 +31,8 @@ import pickle as pkl
 GENE_NUM = 15000 # 16906
 DATA_PATH = 'data/MCDAA_15k.h5ad'  # 'data/Zheng68K.h5ad'
 cell_type = 'cell_type'  # 'celltype'
-EPOCH = 1
+EPOCH = 5
+BATCH_SIZE = 12
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--local_rank", type=int, default=-1, help='Local process rank.')
@@ -39,7 +40,7 @@ parser.add_argument("--bin_num", type=int, default=5, help='Number of bins.')
 parser.add_argument("--gene_num", type=int, default=GENE_NUM, help='Number of genes.')
 parser.add_argument("--epoch", type=int, default=EPOCH, help='Number of epochs.')
 parser.add_argument("--seed", type=int, default=2021, help='Random seed.')
-parser.add_argument("--batch_size", type=int, default=3, help='Number of batch size.')
+parser.add_argument("--batch_size", type=int, default=BATCH_SIZE, help='Number of batch size.')
 parser.add_argument("--learning_rate", type=float, default=1e-4, help='Learning rate.')
 parser.add_argument("--grad_acc", type=int, default=60, help='Number of gradient accumulation.')
 parser.add_argument("--valid_every", type=int, default=1, help='Number of training epochs between twice validation.')
